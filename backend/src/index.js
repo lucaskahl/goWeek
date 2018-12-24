a41fa3bb0f3e37c5850d22fd65dbab50838c1094
Lucas <lucaschangkahl@gmail.com>
2018-12-24 10:36:52 -0200

    Primeiro commit

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+mongoose.connect('mongodb://user:user123@ds141704.mlab.com:41704/learning-backend', {
+  useNewUrlParser: true
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(require('./routes'));
+
+server.listen(3000, () => {
+  console.log('Server online on port 300');
+});
